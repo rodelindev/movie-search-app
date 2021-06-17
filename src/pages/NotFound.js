8e1de5cfccf2c5b4051043feea54d5926becc963
rodelindipre96 <rdbivieca@gmail.com>
2021-06-17 17:12:25 -0400

    404 error component and style.

---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div className="element-status">
+            <h1 className="title">404!</h1>
+            <h3 className="subtitle">Page not found</h3>
+        </div>
+    )
+}
+
+export default NotFound
